a3b6596a384ae2a8644955da69401b5b9e0a7364
meusplugins <152213668+meusplugins@users.noreply.github.com>
2025-11-20 10:19:40 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,11 @@ export default defineConfig(({ mode }) => {
         port: 3000,
         host: '0.0.0.0',
       },
+      preview: {
+        port: 4173,
+        host: '0.0.0.0',
+        allowedHosts: ['especialista.nuzz.com.br'], // 👈 LIBERA O DOMÍNIO
+      },
       plugins: [react()],
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
